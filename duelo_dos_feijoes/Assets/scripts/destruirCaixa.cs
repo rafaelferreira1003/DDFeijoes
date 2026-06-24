b615b27140d658b641a3f2d43c5e3395828953cc
rafaelferreira <rafinhamoraes1003@gmail.com>
2026-06-24 15:52:34 -0300

    sistema de destruição de caixa parcialmente

--- duelo_dos_feijoes/Assets/scripts/destruirCaixa.cs
+++ duelo_dos_feijoes/Assets/scripts/destruirCaixa.cs
@@ -5,17 +5,18 @@ public class destruirCaixa : MonoBehaviour
     public int vida = 5;
     public int dano = 1;
 
-
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag ("player2"))
+        if (other.CompareTag ("tiro"))
         {
             vida = vida - dano;
+            Destroy(other.gameObject);
             if (vida < 0)
-            {
-
-
+            {GetComponent<Animator>().SetBool("destruir_caixa", true);
+                GetComponent<BoxCollider2D>().enabled = false;
             }
+
+            else {GetComponent<Animator>().SetBool("dano_caixa", true);}
 
         }
     }
