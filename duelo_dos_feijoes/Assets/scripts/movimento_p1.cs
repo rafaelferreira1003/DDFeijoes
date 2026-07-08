fb4b6d04e0716c79231fff2383834e99131c2e96
rafaelferreira <rafinhamoraes1003@gmail.com>
2026-07-08 15:48:59 -0300

    sistema de rotacao

--- duelo_dos_feijoes/Assets/scripts/movimento_p1.cs
+++ duelo_dos_feijoes/Assets/scripts/movimento_p1.cs
@@ -9,8 +9,11 @@ public class movimento_p1 : MonoBehaviour
     public GameObject arma;
     public Rigidbody2D rb;
     public bool p1morto = false;
-    public float speed = 5f;
-    public float eixoY = 0;
+    public float velocidade = 5f;
+    public float rotacao = 45f;
+    public float movimento_y;
+    public float rotacao_x;
+    public GameObject imagem;
 
 
     void Start()
@@ -19,16 +22,28 @@ public class movimento_p1 : MonoBehaviour
     }
     void Update()
     {
+
         p1morto = vida.GetComponent<vida>().player1morto;
         if (p1morto == false)
         {
-            eixoY = (Input.GetAxisRaw("Vertical")) * speed;
-            transform.position = new Vector2(transform.position.x, transform.position.y + eixoY * Time.deltaTime);
-            transform.rotation = (arma.transform.rotation);
+            rotacao_x = (Input.GetAxisRaw("Horizontal"));
+            movimento_y = (Input.GetAxisRaw("Vertical"));
+            transform.Translate(0, movimento_y * velocidade * Time.deltaTime, 0);
+            transform.Rotate(0, 0, -rotacao_x * rotacao * Time.deltaTime);
+
+
+            if (movimento_y != 0 || rotacao_x != 0)
+            {
+                imagem.GetComponent<Animator>().SetBool("movendo", true);
+            }
+            else
+            {
+                imagem.GetComponent<Animator>().SetBool("movendo", false);
+            }
         }
         else
         {
-            GetComponent<Animator>().SetTrigger("morto");
+            imagem.GetComponent<Animator>().SetTrigger("morto");
         }
     }
 }
--- duelo_dos_feijoes/Assets/scripts/rotacao_p1.cs
+++ duelo_dos_feijoes/Assets/scripts/rotacao_p1.cs
@@ -1,24 +1,13 @@
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class rotacao_p1 : MonoBehaviour
 {
-    public float velocidaderotacao = 100f;
-    public float eixoX = 0f;
-    public GameObject player1;
-    public Rigidbody2D rig2;
-
-    void Start()
-    {
-        rig2 = GetComponent<Rigidbody2D>();
-    }
-
+    public Vector3 rotacao = Vector3.zero;
     // Update is called once per frame
     void Update()
     {
-
-        eixoX = Input.GetAxis("Horizontal") * velocidaderotacao;
-        transform.RotateAround(player1.transform.position, Vector3.forward, eixoX * Time.deltaTime);
-      
+        transform.rotation = Quaternion.Euler(rotacao);
     }
 }
